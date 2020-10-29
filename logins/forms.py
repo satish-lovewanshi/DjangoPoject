afa008f1ed0719f489b60cb62ae7baab8e86213c
satish lovewanshI <satish@gmail.com>
2020-10-29 16:42:22 +0530

    model form login and registration

--- logins/forms.py
+++ logins/forms.py
@@ -1,13 +1,49 @@
 from django import forms
-from .models import StudentReg,TeacherReg
+from .models import StudentReg,TeacherReg,Branch
 
 class StudentRegForm(forms.ModelForm):
     class Meta:
         model=StudentReg
         fields=['college_code','name','roll_number','branch','year','mobile']
         # labels={}
+        widgets={
+            'college_code': forms.Select(attrs={'class':'form-control','placeholder':'Enter college code'}),
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'roll_number': forms.TextInput(attrs={'class':'form-control'}),
+            'branch': forms.Select(attrs={'class':'form-control'}),
+            'year': forms.Select(attrs={'class':'form-control'}),
+            'mobile': forms.TextInput(attrs={'class':'form-control'}),        
+        }
 class TeacherRegForm(forms.ModelForm):
     
     class Meta:
         model = TeacherReg
-        fields = ()
+        fields='__all__'
+        widgets={
+            'college_code': forms.Select(attrs={'class':'form-control','placeholder':'Enter college code'}),
+            'branch': forms.Select(attrs={'class':'form-control'}),
+            'pass_number': forms.TextInput(attrs={'class':'form-control'}),
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'mobile': forms.TextInput(attrs={'class':'form-control'}),
+        }
+class StudentLoginForm(forms.ModelForm):
+    class Meta:
+        model=StudentReg
+        fields=['college_code','roll_number','mobile']
+        # labels={}
+        widgets={
+            'college_code': forms.Select(attrs={'class':'form-control','placeholder':'Enter college code'}),
+            'roll_number': forms.TextInput(attrs={'class':'form-control'}),
+            'mobile': forms.TextInput(attrs={'class':'form-control'}),        
+        }
+class TeacherLoginForm(forms.ModelForm):
+    class Meta:
+        model = TeacherReg
+        exclude=('branch','name')
+        widgets={
+            'college_code': forms.Select(attrs={'class':'form-control','placeholder':'Enter college code'}),
+            'pass_number': forms.TextInput(attrs={'class':'form-control'}),
+            'mobile': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+    
--- logins/views.py
+++ logins/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . forms import StudentRegForm,TeacherRegForm
+from . forms import StudentRegForm,TeacherRegForm,StudentLoginForm,TeacherLoginForm
 
 def login(request):
-    return render(request,'login.html')
+    StudentLogin=StudentLoginForm()
+    TeacherLogin=TeacherLoginForm()
+    return render(request,'login.html',{'StudentLoginForm':StudentLogin,'TeacherLoginForm':TeacherLogin})
+
 def register(request):
     StudentSignup=StudentRegForm()
-    return render(request,'register.html',{'StudentRegFrom':StudentSignup})
+    TeacherSignup=TeacherRegForm()
+    return render(request,'register.html',{'StudentRegFrom':StudentSignup,'TeacherRegForm':TeacherSignup})
