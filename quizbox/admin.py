8705f1b28ceabbaec8a267b50bc815934ee7b48c
satish lovewanshI <satish@gmail.com>
2020-11-01 23:32:45 +0530

    makign questins form for multiple questions

--- quizbox/admin.py
+++ quizbox/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Test,Question
 
 # Register your models here.
+admin.site.register(Test)
+admin.site.register(Question)
--- quizbox/views.py
+++ quizbox/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .forms import QuestionForm,TestForm
+from .forms import QuestionForm,TestForm,NumberOfQues
+from django.forms import formset_factory
 
 def QuizMake(request):
     form=TestForm()
-    return render(request,'QuizMake.html',{'forms':form})
+    addques=QuestionForm()
+    if request.method=='POST':
+        filled_form=TestForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            filled_form_id=filled_form.save()
+            filled_form_pk=filled_form_id.id
+            count=filled_form_id.number_of_questions
+            PizzaFormSet = formset_factory(QuestionForm,extra=count)
+            formset=PizzaFormSet()
+            return render(request,'QuizMake.html',{'forms':formset,'filled_form_id':filled_form_pk}) 
+    else:
+        return render(request,'QuizMake.html',{'forms':form,'form':addques})
 def AddQues(request):
-    form=QuestionForm()
-    return render(request,'AddQues.html',{'forms':form})  #redierct making 
+    # form=QuestionForm()
+    number_of_ques=0 #set default 0pizzas
+    noq= NumberOfQues(request.GET)
+    if noq.is_valid():
+        number_of_ques = noq.cleaned_data['number']
+    QuesFormSet = formset_factory(QuestionForm,extra=number_of_ques)
+    formset=QuesFormSet()
+
+    return render(request,'QuizMake.html',{'forms':formset})  #redierct making 
