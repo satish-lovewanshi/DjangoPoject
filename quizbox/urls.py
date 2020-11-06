21cc4b1edbbe1dec75b553e153cf42875c9e523e
satish lovewanshI <satish@gmail.com>
2020-11-06 15:44:26 +0530

    all update and delete function working done

--- quizbox/urls.py
+++ quizbox/urls.py
@@ -7,8 +7,14 @@ urlpatterns = [
     path('quizmake',views.QuizMake,name='QuizMake'),
     path('AddQues/<int:pk>',views.AddQues,name='AddQues'),
     path('edit',views.edit,name='edit'),
-    path('manage',views.manage,name='manage'),
+    path('manage_test',views.manage_test,name='manage_test'),
+    path('manage_ques/<int:pk>',views.manage_ques,name='manage_ques'),
+
     path('delete_test/<int:pk>',views.delete_test,name='delete_test'),
-    path('edit_test/<int:pk>',views.edit_test,name='edit_test')
+    path('delete_ques/<int:pk>',views.delete_ques,name='delete_ques'),
+
+    path('edit_test/<int:pk>',views.edit_test,name='edit_test'),
+    path('edit_ques/<int:pk>',views.edit_ques,name="edit_ques"),
+
 
 ]
--- quizbox/views.py
+++ quizbox/views.py
@@ -16,11 +16,7 @@ def QuizMake(request):
             filled_form_pk=filled_form_id.id
             messages.success(request, 'Your test details saved.You can add questions for test click on add question button....')
             note='Test Details Add Successfuly !'
-            return render(request,'QuizMake.html',{'filled_form_pk':filled_form_pk,'note':note})
-        else:
-            note='Something went wrong !'
-            messages.warning(request,'Please go back and fill all field correctly')
-            return render(request,'QuizMake.html',{'note':note})
+        return render(request,'QuizMake.html',{'filled_form_pk':filled_form_pk,'note':note,'forms':test_form})
     else:
         return render(request,'QuizMake.html',{'forms':test_form})
 
@@ -38,7 +34,7 @@ def AddQues(request,pk):
                 post.test=pk
                 post.save()
             messages.success(request,'Your Questions is Saved Successfully')
-            return HttpResponseRedirect('/edit')
+            return HttpResponseRedirect('/manage_test')
         else:
             messages.warning(request,'Fill all field required go back and fill now ')
         # return render(request,'QuizMake.html',{'note':note})
@@ -46,13 +42,41 @@ def AddQues(request,pk):
     return render(request,'QuizMake.html',{'forms':formset})
 def edit(request):
     return render(request,'edit.html')
-def manage(request):
+def manage_test(request):
     test=Test.objects.all()
-    return render(request,'manage.html',{'test':test})
+    return render(request,'manage_test.html',{'test':test})
 def delete_test(request,pk):
     test=Test.objects.get(id=pk).delete()
-    messages.success(request,'delete succesfully ')
-    return HttpResponseRedirect('/edit')
+    messages.success(request,'Test delete succesfully !')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+def delete_ques(request,pk):
+    Question.objects.get(id=pk).delete()
+    messages.success(request,"Question delete successfully !")
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
 def edit_test(request,pk):
-    test=Test.objects.get(id=pk)
-    return render(request,'edit.html',{'test':test})
+    if request.method == 'POST':
+        test=Test.objects.get(pk=pk)
+        test_form=TestForm(request.POST,instance=test)
+        if test_form.is_valid():
+            test_form.save()
+            messages.success(request,"Test deatils is successfully Updated !")
+        return HttpResponseRedirect('/manage_test')
+    else:
+        test=Test.objects.get(pk=pk)
+        test_form=TestForm(instance=test)
+    return render(request,'QuizMake.html',{'test_form':test_form})
+def manage_ques(request,pk):
+    ques=Question.objects.filter(test=pk)
+    return render (request,'manage_ques.html',{'ques':ques})
+def edit_ques(request,pk):
+    if request.method == 'POST':
+        ques=Question.objects.get(id=pk)
+        ques_form=QuestionForm(request.POST,instance=ques)
+        if ques_form.is_valid():
+            ques_form.save()
+            messages.success(request,"Question details is successfully Updated ! ")
+        return HttpResponseRedirect('/manage_test')        
+    else:
+        ques=Question.objects.get(id=pk)
+        ques_form=QuestionForm(instance=ques)
+    return render(request,'QuizMake.html',{'ques_update_form':ques_form})
